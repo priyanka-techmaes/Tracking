ce03f4c49567c704864781aed0757c37daf9b1d8
Priyanka <priyanka.kotkar@imagnate.com>
2012-08-10 18:13:04 +0530

    Js file added by Pradip

---
+++ js/ValidationRules.js
@@ -0,0 +1,40 @@
+﻿//Our validation script will go here.
+
+$(document).ready(function(){
+    
+	$.validator.addMethod("alpha", function(value, element) {
+	return this.optional(element) || value == value.match(/^[a-zA-Z]+$/);
+	},"Only Characters Allowed.");
+	
+	
+	$.validator.addMethod("alphanumericspecial", function(value, element) {
+     return this.optional(element) || value == value.match(/^[-a-zA-Z0-9_ ]+$/);
+     }, "Only letters, Numbers & Space/underscore Allowed.");
+	
+	//validation implementation will go here.
+	$("#loginForm").validate({
+	   /* rules: {
+	        txtTextOnly: {
+	           alpha: true
+	        },
+			
+			password:{
+				alphanumericspecial:true
+			},
+			
+			confirmPassword : {
+				equalTo : "#password" 
+				}
+	    },*/
+		 messages: {
+	         userName:{
+				 required:"Enter User Name"  
+				 	},
+			 password: {
+				required: "Enter Password"
+			}
+			
+		 }
+	
+	});
+})
